fb1c88bf97ed64dbf772334849b67df303df52e7
portiaportia <portiaplante@gmail.com>
2023-05-25 13:00:10 -0400

    close #37

---
+++ DataLoader.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class DataLoader {
+    public static ArrayList<Card> getCards(){
+        ArrayList<Card> cards = new ArrayList<Card>();
+
+        return cards;
+    }
+
+    public static ArrayList<Account> getAccounts(){
+        ArrayList<Account> accounts = new ArrayList<Account>();
+
+        return accounts;
+    }
+}
---
+++ DataWriter.java
@@ -0,0 +1,9 @@
+public class DataWriter {
+   public static boolean saveUsers(){
+    return true;
+   } 
+
+   public static boolean saveCards(){
+    return true;
+   }
+}
