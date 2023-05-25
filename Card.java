cf69b43de4321dc76eabab4c2ebb857a5667631e
Mark <markbob666@gmail.com>
2023-05-25 12:52:43 -0400

    close #1

--- Card.java
+++ Card.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
 public class Card {
-    
-    public Card(){
+    private String playerFirstName;
+    private String playerLastName;
+    private String playerPosition;
+    private String teamName;
+    private ArrayList<String> playerStats;
+    private int cardNumber;
+    private String playerTrivia;
+    private String rarityType;
+    private Boolean isRookie;
+    private ArrayList<User> users;
+    private int numCardsTotal;
+    private int numCardsInInventory;
+    public Card() {
         
+    }
+    public double getValue() {
+        return 0.0;
+    }
+    public User getOwner() {
+        User u = new User();
+        return u;
     }
 }
