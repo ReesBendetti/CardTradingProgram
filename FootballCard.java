44eced3757ad665dc27403c7696bc90a5e67ebc6
Mark <markbob666@gmail.com>
2023-05-25 13:20:50 -0400

    close #3

--- FootballCard.java
+++ FootballCard.java
@@ -1,6 +1,9 @@
-public class FootballCard {
-    public FootballCard() {
+import java.util.ArrayList;
 
+public class FootballCard extends Card{
+    public FootballCard(String playerFirstName, String playerLastName, String playerPosition, String sportsLeague, String teamName, ArrayList<String> playerStats, int cardNumber, String playerTrivia, String companyInfo)
+     {
+        super(playerFirstName, playerLastName, playerPosition, sportsLeague, teamName, null, cardNumber, playerTrivia, companyInfo, null);
     }
     public double getValue() {
         return 0.0;
