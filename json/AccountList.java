cacbb33b38b7b06618692829e2e948f4d9d6b088
ReesBendetti <rtblego@gmail.com>
2023-05-25 13:01:16 -0400

    AccountList

---
+++ json/AccountList.java
@@ -0,0 +1,35 @@
+package json;
+
+import java.util.ArrayList;
+
+public class AccountList {
+    private ArrayList<User> users;
+    private static AccountList accountList;
+
+    private AccountList() {
+        
+    }
+
+    public static AccountList getInstance() {
+        if (accountList == null) {
+            accountList = new AccountList();
+        }
+        return accountList;
+    }
+
+    public boolean addUser(String username, String password, String email) {
+        
+    }
+
+    public boolean removeUser (String username) {
+
+    }
+
+    public User getUser(String username) {
+
+    }
+
+    public boolean editUser(User user, String username, String password, String email) {
+
+    }
+}
