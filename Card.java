d91257dc235cc8cc74d6fd835bab61a743cce9ba
Vindico <107150204+Vinadium@users.noreply.github.com>
2023-05-30 01:46:33 -0400

    dataloader beginnings

--- Card.java
+++ Card.java
@@ -5,7 +5,6 @@ public class Card {
     private String playerLastName;
     private String playerPosition;
     private String teamName;
-    private ArrayList<String> playerStats;
     private int cardNumber;
     private String playerTrivia;
     private String rarityType;
@@ -14,12 +13,14 @@ public class Card {
     private int numCardsTotal;
     private int numCardsInInventory;
     private UUID id;
-    public Card(UUID id, String playerFirstName, String playerLastName, String playerPosition, String sportsLeague, String teamName, ArrayList<String> playerStats, int cardNumber, String playerTrivia, String rarityType, Boolean isRookie, ArrayList<User> users, int numCardsTotal, int numCardsInventory) {
+
+
+    //deleted player stats -Rees
+    public Card(UUID id, String playerFirstName, String playerLastName, String playerPosition, String sportsLeague, String teamName, int cardNumber, String playerTrivia, String rarityType, Boolean isRookie, ArrayList<User> users, int numCardsTotal, int numCardsInventory) {
         this.playerFirstName = playerFirstName;
         this.playerFirstName = playerLastName;
         this.playerPosition = playerPosition;
         this.teamName = teamName;
-        this.playerStats = playerStats;
         this.cardNumber = cardNumber;
         this.playerTrivia = playerTrivia;
         this.rarityType = rarityType;
@@ -32,12 +33,12 @@ public class Card {
 
     }
 
-    public Card(String playerFirstName, String playerLastName, String playerPosition, String sportsLeague, String teamName, ArrayList<String> playerStats, int cardNumber, String playerTrivia, String rarityType, Boolean isRookie, int numCardsTotal, int numCardsInventory) {
+    //deleted player stats -Rees
+    public Card(String playerFirstName, String playerLastName, String playerPosition, String sportsLeague, String teamName, int cardNumber, String playerTrivia, String rarityType, Boolean isRookie, int numCardsTotal, int numCardsInventory) {
         this.playerFirstName = playerFirstName;
         this.playerFirstName = playerLastName;
         this.playerPosition = playerPosition;
         this.teamName = teamName;
-        this.playerStats = playerStats;
         this.cardNumber = cardNumber;
         this.playerTrivia = playerTrivia;
         this.rarityType = rarityType;
--- DataLoader.java
+++ DataLoader.java
@@ -5,33 +5,41 @@ import java.util.UUID;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import java.util.ArrayList;
 
 public class DataLoader extends DataConstants {
-    public static ArrayList<Card> getCards(){
+    public static ArrayList<Card> getCards() {
         ArrayList<Card> cards = new ArrayList<Card>();
 
         try {
-			FileReader reader = new FileReader(CARDS_FILE_NAME);
-			JSONParser parser = new JSONParser();
-			JSONArray cardsJSON = (JSONArray)new JSONParser().parse(reader);
-			
-			for(int i=0; i < cardsJSON.size(); i++) {
-				JSONObject cardJSON = (JSONObject)cardsJSON.get(i);
-                UUID cardID = UUID.fromString((String)cardJSON.get(CARD_ID));
-				String playerFirstName = (String)cardJSON.get(CARD_PLAYER_FIRST_NAME);
-				System.out.println("Test: " + cardID + " " + playerFirstName);
-				
-				//cards.add(new Card(...));
-			}
-			
-			return cards;
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-            return cards;
+            FileReader reader = new FileReader(CARDS_FILE_NAME);
+            JSONParser parser = new JSONParser();
+            JSONArray cardsJSON = (JSONArray) parser.parse(reader);
+
+            for (int i = 0; i < cardsJSON.size(); i++) {
+                JSONObject cardJSON = (JSONObject) cardsJSON.get(i);
+                UUID cardID = UUID.fromString((String) cardJSON.get("id"));
+                String playerFirstName = (String) cardJSON.get("player-first-name");
+                String playerLastName = (String) cardJSON.get("player-last-name");
+                String playerPosition = (String) cardJSON.get("player-position");
+                String sportsLeague = (String) cardJSON.get("sports-league");
+                String teamName = (String) cardJSON.get("team-name");
+                int cardNumber = Integer.parseInt(String.valueOf(cardJSON.get("card-number")));
+                String playerTrivia = (String) cardJSON.get("player-trivia");
+                String rarityType = (String) cardJSON.get("rarity type");
+                boolean isRookie = (boolean) cardJSON.get("is-rookie");
+                int numberTotalCards = Integer.parseInt(String.valueOf(cardJSON.get("number-total-cards")));
+                int totalCardsInventory = Integer.parseInt(String.valueOf(cardJSON.get("total-cards-inventory")));
+
+                // I think I still need to add cardID but not sure how to implement it into the cards.json
+                Card card = new Card(playerFirstName, playerLastName, playerPosition, sportsLeague, teamName, cardNumber, playerTrivia, rarityType, isRookie, numberTotalCards, totalCardsInventory);
+                cards.add(card);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        return cards;
     }
 
     public static ArrayList<Account> getAccounts(){
