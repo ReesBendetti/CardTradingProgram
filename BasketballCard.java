6045f1f12b8d3b227d8592a4cdd8a61f3b3f56cf
ReesBendetti <rtblego@gmail.com>
2023-05-30 13:01:26 -0400

    close Update Basketball and Football Constructors #41

--- BasketballCard.java
+++ BasketballCard.java
@@ -3,17 +3,12 @@ import java.util.UUID;
 
 public class BasketballCard extends Card {
     
-<<<<<<< HEAD
-    public BasketballCard(String playerFirstName, String playerLastName, String playerPosition, String sportsLeague, String teamName, ArrayList<String> playerStats, int cardNumber, String playerTrivia, String companyInfo) {
-        super(playerFirstName, playerLastName, playerPosition, sportsLeague, teamName, playerStats, cardNumber, playerTrivia, companyInfo, null, null, cardNumber, cardNumber);
-=======
     public BasketballCard(UUID id, String playerFirstName, String playerLastName, String playerPosition, String sportsLeague, String teamName, ArrayList<String> playerStats, int cardNumber, String playerTrivia, String rarityType, Boolean isRookie, ArrayList<User> users, int numCardsTotal, int numCardsInventory) {
-        super(id, playerFirstName, playerLastName, playerPosition, sportsLeague, teamName, playerStats, cardNumber, playerTrivia, rarityType, isRookie, users, numCardsTotal, numCardsInventory);
->>>>>>> 799b4046453422d1f84eaa8ecbfa0a48008e91b6
+        super(id, playerFirstName, playerLastName, playerPosition, sportsLeague, teamName, cardNumber, playerTrivia, rarityType, isRookie, numCardsTotal, numCardsInventory);    
     }
 
     public BasketballCard(String playerFirstName, String playerLastName, String playerPosition, String sportsLeague, String teamName, ArrayList<String> playerStats, int cardNumber, String playerTrivia, String rarityType, Boolean isRookie, int numCardsTotal, int numCardsInventory) {
-        super(playerFirstName, playerLastName, playerPosition, sportsLeague, teamName, playerStats, cardNumber, playerTrivia, rarityType, isRookie, numCardsTotal, numCardsInventory);
+        super(playerFirstName, playerLastName, playerPosition, sportsLeague, teamName, cardNumber, playerTrivia, rarityType, isRookie, numCardsTotal, numCardsInventory);
     }
 
     public double getValue() {
